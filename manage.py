5af3bf310a647420c4d3b185e0e94b81dc324b4b
Amuse <amusesla@gmail.com>
2020-12-20 23:27:03 +0900

    2.Initial_setup

--- manage.py
+++ manage.py
@@ -1,6 +1,20 @@
+#from app import create_app
+#
+#
+#if __name__ == '__main__':
+#    app = create_app()
+#
+#    app.runserver(host='localhost', port=5000)
+
+
+
+from flask_script import Manager
+from flask_script.commands import Server
+
 from app import create_app
 
-if __name__ == '__main__':
-    app = create_app()
+manager = Manager(create_app)
+manager.add_command('runserver', Server())
 
-    app.run(host='localhost', port=5000)
+if __name__ =='__main__':
+    manager.run()
